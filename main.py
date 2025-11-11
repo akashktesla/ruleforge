2d7b186cf9563cfa16fae90a0aba3e17620775d1
akashktesla <akashktesla@gmail.com>
2025-11-11 14:42:15 +0530

    :)

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import pandas as pd
 
+
+def clean_with_policy(df,policy):
+    print(f"policy: {policy}")
 
 
 if __name__ == "__main__":
-    policy = {}
-    
+    df = pd.read_csv("datasets/spaceship-titanic/train.csv");
+    print(df.head())
+    policy = [{
+                  "column":"akash",
+              },]
+
+
 
