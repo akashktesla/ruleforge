9c9e2153df6f5c6757471b1266119b428e9e1e59
akashktesla <akashktesla@gmail.com>
2025-11-11 16:21:32 +0530

    :)

--- main.py
+++ main.py
@@ -8,27 +8,48 @@ def enforce_type(value,value_name,type):
 
 class ColumnPolicy:
     def __init__(self):
-        return None
+        pass
+
     def name(self,name:str):
-        enforce_type(value = name,value_name="name",type= str)
+        enforce_type(value = name,value_name="Name",type=str)
         self.name = name
         return self
-    def type(self,type):
-        self.type = type
+
+    def type(self,type_):
+        enforce_type(value = type_,value_name="Type",type=type)
+        self.type = type_
         return self
 
-def clean_with_policy(df,policy):
-    print(f"policy: {policy}")
+class RuleForger:
+    def __init__(self,df):
+        self.df = df
+        self.column_policies = []
+    def add_column_policy(self,policy):
+        self.column_policies.append(policy)
+
+    def validate(self):
+        for policy in self.column_policies:
+            self.validate_column_policy(policy)
+    def validate_column_policy(self,policy):
+        for i in df[policy.name]:
+            #Validate Type
+            if type(i) != policy.type:
+                print(f"Type {type(i)}, Required: {policy.type}")
+                print(f"Mismatch found: {i}")
+
+
 
 
 if __name__ == "__main__":
     df = pd.read_csv("datasets/spaceship-titanic/train.csv");
     print(df.head())
+    rf = RuleForger(df)
     policy = ColumnPolicy()\
             .name("HomePlanet")\
             .type(str)
+    rf.add_column_policy(policy)
+    rf.validate()
 
-    print(policy.name)
 
     # policy = [{
     #               "column":"HomePlanet",
