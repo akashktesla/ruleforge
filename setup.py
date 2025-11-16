73d25017c4943609d80a81cf15652a1a1cd9e85b
akashktesla <akashktesla@gmail.com>
2025-11-17 01:35:28 +0530

    :)

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
-
 setup(
 name="ruleforge",
 version="0.0.1",
 packages=["ruleforge"],
-ext_modules=cythonize([], language_level=3),
+install_requires = ["pandas"],
+ext_modules=cythonize(["ruleforge/validators.pyx"], language_level=3),
 )
